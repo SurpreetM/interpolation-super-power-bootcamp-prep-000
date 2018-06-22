1a9965ce3a41b3dc0def475303e3537939b7ab8a
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-22 15:06:40 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,7 @@
 # Write your #display_rainbow method here
-def display_rainbow (rainbow)
+def display_rainbow (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  
+  puts 
 
 end 
 
-rainbow = []
